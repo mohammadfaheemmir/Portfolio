310ec716093eb17744b84dae55e07d95daeeef4f
Faheem <138996989+mohammadfaheemmir@users.noreply.github.com>
2024-01-10 21:03:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const menu_items = document.querySelectorAll('nav .mainMenu li a');
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+// close menu when you click on a menu item 
+menu_items.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
+
+// var counter = 1;
+// setInterval(function () {
+//     document.getElementById('radio' + counter).checked = true;
+//     counter++;
+//     if (counter > 4) {
+//         counter = 1;
+//     }
+// }, 3000);
+
+
+let i = 0; // current slide
+let j = 4; // total slides
+
+const dots = document.querySelectorAll(".dot-container button");
+const images = document.querySelectorAll(".image-container img");
+
+function indicator(num){
+    dots.forEach(function(dot){
+        dot.style.backgroundColor = "transparent";
+    });
+    document.querySelector(".dot-container button:nth-child(" + num + ")").style.backgroundColor = "#8052ec";
+}
+
+function dot(index){
+    images.forEach(function(image){
+        image.classList.remove("active");
+    });
+    document.getElementById("content" + index).classList.add("active");
+    i = index - 1;
+    indicator(index);
+}
+
